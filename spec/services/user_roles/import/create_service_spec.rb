38c52c9bea67139c4c778ce3f1139e0d1da8f11b
Jonathan Rojas <jonathan.rojas@rankmi.com>
2023-07-31 18:13:20 -0500

    se estan creando los test para la carga de roles de forma masiva

--- spec/services/user_roles/import/create_service_spec.rb
+++ spec/services/user_roles/import/create_service_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe UserRoles::Import::CreateService do
     end
 
     it 'return error the file is not xlsx' do
-      file =  Rack::Test::UploadedFile.new('./spec/files/user_roles/templates/import-user-roles-header-without-roles.csv',
+      file =  Rack::Test::UploadedFile.new('./spec/files/user_roles/templates/1-extension-error.csv',
                                                    ' application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
 
       service = described_class.new(enterprise: enterprise, user: user, file: file)
@@ -20,7 +20,7 @@ RSpec.describe UserRoles::Import::CreateService do
     end
 
     it 'return error the header has not roles' do
-      file = Rack::Test::UploadedFile.new('./spec/files/user_roles/templates/import-user-roles-header-without-roles.xlsx',
+      file = Rack::Test::UploadedFile.new('./spec/files/user_roles/templates/2-error-without-roles-header.xlsx',
                                               ' application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
 
       service = described_class.new(enterprise: enterprise, user: user, file: file)
@@ -28,11 +28,68 @@ RSpec.describe UserRoles::Import::CreateService do
     end
 
     it 'return error the header has not identifier' do
-      file = Rack::Test::UploadedFile.new('./spec/files/user_roles/templates/import-user-roles_header_without_cc.xlsx',
+      file = Rack::Test::UploadedFile.new('./spec/files/user_roles/templates/3-error-without-identifier-header.xlsx',
                                           ' application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
 
       service = described_class.new(enterprise: enterprise, user: user, file: file)
       expect { service.perform }.to raise_error(ArgumentError)
+    end
+
+    it 'return error the header identifier' do
+      file = Rack::Test::UploadedFile.new('./spec/files/user_roles/templates/4-error-identifier-header.xlsx',
+                                          ' application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+
+      service = described_class.new(enterprise: enterprise, user: user, file: file)
+      expect { service.perform }.to raise_error(ArgumentError)
+    end
+
+    it 'return error the header role not found' do
+      file = Rack::Test::UploadedFile.new('./spec/files/user_roles/templates/5-error-role-not-found.xlsx',
+                                          ' application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+
+      service = described_class.new(enterprise: enterprise, user: user, file: file)
+      expect { service.perform }.to raise_error(ArgumentError)
+    end
+
+    it 'finish with errors' do
+      file = Rack::Test::UploadedFile.new('./spec/files/user_roles/templates/6-finish.xlsx',
+                                          ' application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+
+      service = described_class.new(enterprise: enterprise, user: user, file: file)
+      service.perform
+
+      expect(service.errors.empty?).to eq(false)
+    end
+
+    it 'finish success!!!' do
+      file = Rack::Test::UploadedFile.new('./spec/files/user_roles/templates/6-finish.xlsx',
+                                          ' application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+
+      sign_up
+
+      user_identifier = User.find_by(identifier: '12345678')
+      expect(user_identifier.roles.size == 1).to eq(true)
+
+      service = described_class.new(enterprise: enterprise, user: user, file: file)
+      service.perform
+
+      expect(service.errors.empty?).to eq(true)
+      expect(user_identifier.roles.size == 3).to eq(true)
+    end
+
+    it 'finish skip duplicate roles' do
+      file = Rack::Test::UploadedFile.new('./spec/files/user_roles/templates/6-finish.xlsx',
+                                          ' application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+
+      user_role_admin
+      user_identifier = User.find_by(identifier: '12345678')
+      expect(user_identifier.roles.size == 2).to eq(true)
+
+      service = described_class.new(enterprise: enterprise, user: user, file: file)
+      service.perform
+
+      expect(service.errors.empty?).to eq(true)
+      expect(user_identifier.roles.size == 3).to eq(true)
     end
   end
 end
--- spec/support/user_roles/user_roles_templates_import_stuff.rb
+++ spec/support/user_roles/user_roles_templates_import_stuff.rb
@@ -13,4 +13,25 @@ shared_context 'user_roles_templates_import_stuff' do
   let!(:role4){ FactoryBot.create(:role, :role_admin) }
 
   let(:user_role_admin) { FactoryBot.create(:user_role, user_id: user.id, role_id: role4.id) }
+
+  let(:sign_up) {
+    allowed_params = {
+      name: FFaker::Name.first_name,
+      lastname: FFaker::Name.last_name,
+      identifier: "12345678",
+      email: "#{FFaker::Name.first_name}.#{20 + Random.rand(11)}#{20 + Random.rand(11)}#{20 + Random.rand(11)}@community.com",
+      address: "T4, P11, A1102",
+      phone: "3174131149",
+      password: 'test123',
+      password_confirmation: 'test123'
+    }
+
+    ::Users::SignUpService.new(enterprise: enterprise, data: allowed_params).call
+  }
+
+  let(:user_role_admin){
+    u = sign_up
+    FactoryBot.create(:user_role, user_id: u.id, role_id: role4.id)
+    u
+  }
 end
