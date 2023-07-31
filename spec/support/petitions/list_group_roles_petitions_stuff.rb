78d395a78cdc8b7c2b4a98f98ffeb78d02c2cf49
Jonathan Rojas <jonathan.rojas@rankmi.com>
2023-07-31 12:33:23 -0500

    creando test

--- spec/support/petitions/list_group_roles_petitions_stuff.rb
+++ spec/support/petitions/list_group_roles_petitions_stuff.rb
@@ -21,23 +21,23 @@ shared_context 'list_group_roles_petitions_stuff' do
     group
   }
 
-  # let(:group_role_admin) {
-  #   group = FactoryBot.create(:group_role, :admin)
-  #
-  #   FactoryBot.create(:group_role_relation, role: role4, group_role: group)
-  #
-  #   group
-  # }
+  let(:group_role_admin) {
+    group = FactoryBot.create(:group_role, :admin)
+
+    FactoryBot.create(:group_role_relation, role: role4, group_role: group)
+
+    group
+  }
 
   let(:enterprise) { enterprise_helper }
   let(:user) { current_user }
   let(:user_enterprise) { user_enterprise_helper }
-  # let(:user_role_coexistence_member) { FactoryBot.create(:user_role, user_id: user.id, role_id: role1.id) }
-  # let(:user_role_committee_member) { FactoryBot.create(:user_role, user_id: user.id, role_id: role2.id) }
+  let(:user_role_coexistence_member) { FactoryBot.create(:user_role, user_id: user.id, role_id: role1.id) }
+  let(:user_role_committee_member) { FactoryBot.create(:user_role, user_id: user.id, role_id: role2.id) }
   let(:user_role_owner_admin) { FactoryBot.create(:user_role, user_id: user.id, role_id: role3.id) }
-  # let(:user_role_admin) { FactoryBot.create(:user_role, user_id: user.id, role_id: role4.id) }
+  let(:user_role_admin) { FactoryBot.create(:user_role, user_id: user.id, role_id: role4.id) }
 
-  # let(:user_2) { FactoryBot.create(:user) }
-  # let(:user_enterprise_2) {  FactoryBot.create(:user_enterprise, user_id: user_2.id, enterprise_id: enterprise_helper.id, active: true) }
-  # let(:user_2_role_owner_admin) { FactoryBot.create(:user_role, user_id: user_2.id, role_id: role3.id) }
+  let(:user_2) { FactoryBot.create(:user) }
+  let(:user_enterprise_2) {  FactoryBot.create(:user_enterprise, user_id: user_2.id, enterprise_id: enterprise_helper.id, active: true) }
+  let(:user_2_role_owner_admin) { FactoryBot.create(:user_role, user_id: user_2.id, role_id: role3.id) }
 end
