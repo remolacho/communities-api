eb97e366260bb51c78ec25fc09aa4b79defa58ff
Jonathan Rojas <jonathan.rojas@rankmi.com>
2023-08-08 08:39:19 -0500

    Se agrega el uso de los archivos desde dev

--- app/models/concerns/answers_petitions/file_routable.rb
+++ app/models/concerns/answers_petitions/file_routable.rb
@@ -5,7 +5,7 @@ module AnswersPetitions
     extend ActiveSupport::Concern
 
     def file_url(file, enterprise_subdomain)
-      return file.url if Rails.env.production?
+      return file.url unless Rails.env.test?
 
       url_path = Rails.application.routes.url_helpers.rails_blob_path(file, only_path: true)
       "#{ENV['BASE_HOST']}#{url_path}?enterprise_subdomain=#{enterprise_subdomain}"
