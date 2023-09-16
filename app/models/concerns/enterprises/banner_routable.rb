652365a3b6e6e3e7ba2e759e1e122488b9c814d9
remolacho <jonathang_25@gmail.com>
2023-09-16 15:54:53 -0500

    Se agrega mejora para decidir a que storage apuntar en local y no usar amazon en local

--- app/models/concerns/enterprises/banner_routable.rb
+++ app/models/concerns/enterprises/banner_routable.rb
@@ -7,10 +7,10 @@ module Enterprises
     def banner_url
       return unless banner.attached?
 
-      return banner.url unless Rails.env.test?
+      return banner.url unless Rails.env.test? || ENV['LOCAL_STORAGE'].eql?('local')
 
       url_path = Rails.application.routes.url_helpers.rails_blob_path(banner, only_path: true)
-      "#{ENV['BASE_HOST']}#{url_path}?enterprise_subdomain=#{enterprise.subdomain}"
+      "#{ENV['BASE_HOST']}#{url_path}?enterprise_subdomain=#{subdomain}"
     end
   end
 end
